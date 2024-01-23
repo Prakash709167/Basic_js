5eb5e5971b7e508bea30aecb1145599e59c73f35
Prakash709167 <prakash709167@gmail.com>
2024-01-23 18:38:12 +0530

    commited

---
+++ 02Array/Part2.js
@@ -0,0 +1,28 @@
+const marvel_heros = ["Ironman", " thor", "Spiderman"];
+const dc_heros = ["Superman", "flash", "Batman"];
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const newHeros = marvel_heros.concat(dc_heros);
+// console.log(newHeros);
+
+
+// const All_heros =[...marvel_heros, ...dc_heros];
+// console.log(All_heros);
+
+// const other_array = [1,2,3,[4,5,6],7,[4,5,[7,8,8]]];
+// const another = other_array.flat(Infinity);
+
+// console.log(another);
+
+
+// console.log(Array.isArray("Prakash"));
+// console.log(Array.from("Prakash"));
+// console.log(Array.from({name: "Prakash"}));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1,score2,score3));
