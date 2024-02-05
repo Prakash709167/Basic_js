f0a2a78dc5e9528f9ddb7a5cf127963d679941d7
Prakash709167 <prakash709167@gmail.com>
2024-02-05 22:47:03 +0530

    commited

---
+++ 04Function/1.js
@@ -0,0 +1,20 @@
+function myName(){
+    console.log("P");
+    console.log("R");
+    console.log("A");
+    console.log("K");
+    console.log("A");
+    console.log("S");
+    console.log("H");
+}
+// myName();
+
+// function add2Number(num1,num2){
+//     console.log(num1+num2);
+// }
+// add2Number(3,4); 
+function add2Number(num1,num2){
+    return num1+num2;
+}
+const result = add2Number(3,5);
+console.log(result);
