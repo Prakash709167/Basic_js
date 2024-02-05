63b803b20b45b1bd40b0bed05420105b6fa90247
Prakash709167 <prakash709167@gmail.com>
2024-02-05 22:29:01 +0530

    commited

---
+++ 03Object/Part3.js
@@ -0,0 +1,10 @@
+const course = {
+    courseName : "js_hindi",
+    price : "999",
+    courseInstructor : "Hitesh",
+}
+
+const {courseInstructor : cI} = course;
+// console.log(courseInstructor);
+console.log(cI);
+console.log(course);
